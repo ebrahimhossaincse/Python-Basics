84964ad430692a1d7e8ebfa15608553d9c2f7a0f
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-05-31 10:10:27 -0400

    update loops examples

--- venv/Fundamentals/String/StringConcatenation.py
+++ venv/Fundamentals/String/StringConcatenation.py
@@ -4,18 +4,18 @@ Using the '+' operator
 greeting = "Hello"
 name = "Ebrahim Hossain"
 full_greeting = greeting + ", " + name + "!"
-print(full_greeting)  # Output: Hello, Alice!
+print(full_greeting)  # Output: Hello, Ebrahim Hossain!
 
 
 '''
 Using the 'str.format()' method
 '''
 full_greeting = "{}, {}!".format(greeting, name)
-print(full_greeting)  # Output: Hello, Alice!
+print(full_greeting)  # Output: Hello, Ebrahim Hossain!
 
 
 '''
 Using f-strings (Python 3.6+)
 '''
 full_greeting = f"{greeting}, {name}!"
-print(full_greeting)  # Output: Hello, Alice!
+print(full_greeting)  # Output: Hello, Ebrahim Hossain!
